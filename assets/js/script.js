eb4d8c4e5d5d236bf991f45e334ddda46597d992
LVinicius <botlink2030@gmail.com>
2022-11-03 20:59:03 -0300

    #

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,10 @@ const input = document.querySelector(".input__search");
 const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
 const btnShiny = document.querySelector(".shiny")
+const btnMale = document.querySelector(".male")
+const btnFemale = document.querySelector(".female")
 
-let searchPokemon = 1, bShiny = false
+let searchPokemon = 1, bShiny = false, male = true, female = false
 
 async function fetchPokemon(pokemon){
 
@@ -35,18 +37,46 @@ async function renderPokemon(pokemon){
         pokemonName.innerHTML = data.name;
         pokemonNumber.innerHTML = data.id;
         searchPokemon = data.id;
-    
-        sprite = data.sprites.versions['generation-v']['black-white'].animated.front_default;
-        if(bShiny){
-            sprite = data.sprites.versions['generation-v']['black-white'].animated.front_shiny;
+        if(male){
+            sprite = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+            if(bShiny){
+                sprite = data.sprites.versions['generation-v']['black-white'].animated.front_shiny;
+            }
+        }else if(female){
+            sprite = data.sprites.versions['generation-v']['black-white'].animated.front_female;
+            if(sprite == null){
+                sprite = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+            }
+            if(bShiny){
+                sprite = data.sprites.versions['generation-v']['black-white'].animated.front_shiny_female;
+                if(sprite == null){
+                    sprite = data.sprites.versions['generation-v']['black-white'].animated.front_shiny;
+                }
+            }
         }
+        
         pokemonImage.style.height = '18%';
         pokemonImage.style.bottom = '55%';
         if(sprite == null){
-            sprite = data.sprites.front_default;
-            if(bShiny){
-                sprite = data.sprites.front_shiny;
+            if(male){
+                sprite = data.sprites.front_default;
+                if(bShiny){
+                    sprite = data.sprites.front_shiny;
+                }
+            }else if(female){
+                sprite = data.sprites.front_female;
+                if(sprite == null){
+                    sprite = data.sprites.front_default;
+                }
+                if(bShiny){
+                    sprite = data.sprites.front_shiny_female;
+                    if(sprite == null){
+                        sprite = data.sprites.front_shiny;
+                    }
+                }
+                
             }
+            
             pokemonImage.style.height = '25%';
             pokemonImage.style.bottom = '52.5%';
     
@@ -98,4 +128,23 @@ btnShiny.addEventListener('click', ()=>{
     renderPokemon(searchPokemon)
 })
 
+btnMale.addEventListener('click', ()=>{
+    if(!male){
+        male = true
+        female = false
+        btnMale.style.scale = '1.1'
+        btnFemale.style.scale = '1'
+    }
+    renderPokemon(searchPokemon)
+})
+
+btnFemale.addEventListener('click', ()=>{
+    if(!female){
+        male = false
+        female = true
+        btnMale.style.scale = '1'
+        btnFemale.style.scale = '1.1'
+    }
+    renderPokemon(searchPokemon)
+})
 renderPokemon(searchPokemon)

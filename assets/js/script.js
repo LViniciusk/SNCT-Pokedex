0b0cf2c5e660bd21b9bf6613de366b1b6a00f2cb
LVinicius <botlink2030@gmail.com>
2022-11-03 14:08:25 -0300

    #

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+const pokemonName = document.querySelector(".pokemon__name");
+const pokemonNumber = document.querySelector(".pokemon__number");
+const pokemonImage = document.querySelector(".pokemon__image");
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input__search");
+
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+
+let searchPokemon = 1
+
+async function fetchPokemon(pokemon){
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if (APIResponse.status == 200){
+        const data = await APIResponse.json();
+        return data;
+    }
+
+    
+}
+
+async function renderPokemon(pokemon){
+    pokemonName.innerHTML = 'Loading...'
+    pokemonNumber.innerHTML = ''
+    pokemonImage.style.display = 'none'
+    const data = await fetchPokemon(pokemon);
+
+    if (data){
+        pokemonImage.style.display='block';
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        searchPokemon = data.id;
+    
+        sprite = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+        pokemonImage.style.height = '18%';
+        pokemonImage.style.bottom = '55%';
+        if(sprite == null){
+            sprite = data.sprites.front_default;
+            pokemonImage.style.height = '25%';
+            pokemonImage.style.bottom = '52.5%';
+    
+            if(sprite == null){
+                sprite = 'assets/imagens/incog.png';
+                pokemonImage.style.height = '18%';
+                pokemonImage.style.bottom = '55%';
+            }
+        }
+        pokemonImage.src = sprite
+        input.value = ''
+    }else{
+        pokemonImage.style.display = 'none'
+        pokemonNumber.innerHTML = '404'
+        pokemonName.innerHTML = 'Not Found :('
+    }
+
+    
+}
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+
+    renderPokemon(input.value.toLowerCase())
+    
+})
+
+btnPrev.addEventListener('click', ()=>{
+    if(searchPokemon > 1){
+        searchPokemon-=1
+        renderPokemon(searchPokemon)
+    }
+})
+btnNext.addEventListener('click', ()=>{
+    if(searchPokemon < 905){
+        searchPokemon+=1;
+        renderPokemon(searchPokemon)
+    }
+})
+
+renderPokemon(searchPokemon)
